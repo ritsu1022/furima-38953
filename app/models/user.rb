ab1a600feedac01e5955fee5f5b1eceaf5d7fab3
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-06-14 23:54:26 +0900

    エラーメッセージ実装、バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,19 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid.Include both letters and numbers'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid.Input full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ-ー]+\z/, message: 'is invalid.Input full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
   has_many  :items
   has_many  :comments
   has_many  :histories
