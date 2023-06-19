5cfaea80e0ee1b565c82ef12a317d4de666bd97f
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-06-19 23:38:55 +0900

    itemモデル、バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,14 @@
 class Item < ApplicationRecord
-
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :region_id, presence: true
+  validates :lead_time_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 
   belongs_to    :user
+  has_one_attached :image
 end
