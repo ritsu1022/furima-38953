266199752a5fda52827767c3ea42a8f7f3fbe98e
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-06-21 19:17:33 +0900

    Revert "Revert "Categoryモデル修正""
    This reverts commit 4f1962dd6c914f20c63732135a81c9ea6e33d6d6.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    
+    @items = Item.order("created_at DESC")
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_fee_id, presence: true
   validates :region_id, presence: true
   validates :lead_time_id, presence: true
@@ -13,5 +13,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :category
+  belongs_to :condition
 end
