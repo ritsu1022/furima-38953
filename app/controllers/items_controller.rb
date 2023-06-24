52f712eb7db04fca51eaac3787a656efa22389c3
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-06-24 21:19:58 +0900

    itemsコントローラー、index、new、createアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
@@ -8,6 +8,20 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_fee_id, :region_id, :lead_time_id, :price, :user, :image).merge(user_id: current_user.id)
   end
 
 end
