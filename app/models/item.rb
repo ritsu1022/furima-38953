2fdd77f9eb1524d7c048f98470fed8bb9f9d0bcc
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-06-24 21:19:01 +0900

    itemモデルバリデーション、アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,10 @@ class Item < ApplicationRecord
   validates :explanation, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_id, presence: true
-  validates :region_id, presence: true
-  validates :lead_time_id, presence: true
-  validates :price, presence: true
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :region_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :lead_time_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }, format: { with: /\A[0-9]+\z/ }
   validates :image, presence: true
 
   belongs_to    :user
@@ -16,4 +16,7 @@ class Item < ApplicationRecord
 
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :region
+  belongs_to :lead_time
 end
