215baf7abe7862323b3cd47a16df4a1d69aca491
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-06-25 19:27:43 +0900

    商品出品機能、プルリク修正①

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,14 @@
 class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :explanation, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true, numericality: { message: "can't be blank", allow_blank: true }
-  validates :condition_id, presence: true, numericality: { message: "can't be blank", allow_blank: true }
-  validates :shipping_fee_id, presence: true, numericality: { message: "can't be blank", allow_blank: true }
-  validates :region_id, presence: true, numericality: { message: "can't be blank", allow_blank: true }
-  validates :lead_time_id, presence: true, numericality: { message: "can't be blank", allow_blank: true }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank", allow_blank: true }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank", allow_blank: true }
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank", allow_blank: true }
+  validates :region_id, presence: true, numericality: { other_than: 1, message: "can't be blank", allow_blank: true }
+  validates :lead_time_id, presence: true, numericality: { other_than: 1, message: "can't be blank", allow_blank: true }
   validates :price, presence: true,
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range', allow_blank: true }, format: { with: /\A[0-9]+\z/, allow_blank: true }
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true, message: 'is out of setting range', allow_blank: true },
+                    format: { with: /\A[0-9]+\z/, allow_blank: true }
   validates :image, presence: true
 
   belongs_to :user
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,8 +37,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it '商品の状態が空では出品できない' do
         @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.condition_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
@@ -47,8 +57,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.shipping_fee_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
       it '発送元の地域が空では出品できない' do
         @item.region_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region can't be blank")
+      end
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.region_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include("Region can't be blank")
       end
@@ -57,15 +77,40 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Lead time can't be blank")
       end
-      it '販売価格が空では出品できない' do
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.lead_time_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Lead time can't be blank")
+      end
+      it '価格が空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = '３000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it '価格が300円未満では出品できない' do
+        @item.price = '250'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it '価格が9_999_999円を超えると出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
       it '商品画像が空では出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
