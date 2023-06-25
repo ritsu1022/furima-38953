e727222b90e4bb302c9ccc6b2b3d70d8dfee1c60
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-06-25 15:42:08 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,16 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
-  validates :explanation, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :region_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :lead_time_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }, format: { with: /\A[0-9]+\z/ }
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :explanation, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true, numericality: { message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { message: "can't be blank" }
+  validates :shipping_fee_id, presence: true, numericality: { message: "can't be blank" }
+  validates :region_id, presence: true, numericality: { message: "can't be blank" }
+  validates :lead_time_id, presence: true, numericality: { message: "can't be blank" }
+  validates :price, presence: true,
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }, format: { with: /\A[0-9]+\z/ }
   validates :image, presence: true
 
-  belongs_to    :user
+  belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
