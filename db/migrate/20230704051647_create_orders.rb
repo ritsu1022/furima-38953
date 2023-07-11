cf80c87265846af2e16670dc93a44dd50f1e2066
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-07-11 16:57:40 +0900

    マイグレーションファイル修正

--- db/migrate/20230704051647_create_orders.rb
+++ db/migrate/20230704051647_create_orders.rb
@@ -7,7 +7,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.string       :house_number,   null: false
       t.string       :building_name
       t.string       :phone_number,   null: false
-      t.references   :user,           null: false, foreign_key: true
+      t.references   :history,        null: false, foreign_key: true
       t.timestamps
     end
   end
